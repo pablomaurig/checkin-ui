8f89388c74a99a8e210412f3ad38784ce182a903
Pablo Maurig <33431223+pablomaurig@users.noreply.github.com>
2022-05-12 10:36:46 -0300

    Agrego Checkout y encuesta (#6)

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -9,6 +9,13 @@ import {
   Input,
   Image,
   Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
 } from '@chakra-ui/react';
 import * as Yup from 'yup';
 import { Formik, Form, Field, FieldArray, getIn } from 'formik';
@@ -16,6 +23,49 @@ import { BookingState } from '../../types/booking.types';
 import { useRef, useState } from 'react';
 import { MdDelete } from 'react-icons/md';
 import { IoPersonAddSharp } from 'react-icons/io5';
+import { ImStarFull } from 'react-icons/im';
+
+const SURVEY = ['Sevicio 1', 'Sevicio 2', 'Sevicio 3', 'Sevicio 4'];
+
+const Survey = ({ onClick, step }: any) => {
+  const [value, setValue] = useState(0);
+  const handleChange = (number: number) => {
+    if (step <= SURVEY.length) {
+      setValue(number);
+      setTimeout(() => {
+        if (step !== SURVEY.length) {
+          setValue(0);
+          onClick();
+        }
+      }, 1000);
+    }
+  };
+
+  const Star = ({ starNumber }: { starNumber: number }) => {
+    return (
+      <Icon
+        h={7}
+        w={7}
+        color={value >= starNumber ? 'yellow.300' : 'gray.200'}
+        as={ImStarFull}
+        onClick={() => handleChange(starNumber)}
+      />
+    );
+  };
+
+  return (
+    <Box>
+      <Text>{SURVEY[step - 1]}</Text>
+      <Flex justifyContent={'space-around'}>
+        <Star starNumber={1} />
+        <Star starNumber={2} />
+        <Star starNumber={3} />
+        <Star starNumber={4} />
+        <Star starNumber={5} />
+      </Flex>
+    </Box>
+  );
+};
 
 const PreviewImage = ({ file }: any) => {
   const [preview, setPreview] = useState<ArrayBuffer | string>('');
@@ -88,9 +138,20 @@ const RequestBookingSchema = Yup.object().shape({
 });
 
 const Home = () => {
-  const [bookingState, setBookingState] = useState(BookingState.Initial);
+  const [bookingState, setBookingState] = useState(BookingState.Active);
+  const [surveyStep, setSurveyStep] = useState(0);
   const [lastName, setLastName] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const FRIENDS_COUNT = 1;
+
+  const handleNextSurveyStep = () => {
+    setSurveyStep(prevstate => prevstate + 1);
+  };
+
+  const handleFinishSurvey = () => {
+    onClose();
+    setBookingState(BookingState.Finalized);
+  };
 
   const checkinSchema = Yup.object().shape({
     guests: Yup.array()
@@ -247,7 +308,7 @@ const Home = () => {
               onSubmit={(values, actions) => {
                 setTimeout(() => {
                   actions.setSubmitting(false);
-                  setBookingState(BookingState.Finalized);
+                  setBookingState(BookingState.Active);
                 }, 1000);
               }}
             >
@@ -399,6 +460,59 @@ const Home = () => {
           </Box>
         </Flex>
       )}
+      {bookingState === BookingState.Active && (
+        <Flex
+          direction={'column'}
+          justifyContent={'space-between'}
+          height={'100%'}
+          flex={'1'}
+        >
+          <Text>Checkin realizado con éxito.</Text>
+          <Button onClick={onOpen}>Realizar checkout</Button>
+          <Modal
+            onClose={onClose}
+            closeOnOverlayClick={false}
+            size={'xs'}
+            isOpen={isOpen}
+            isCentered
+          >
+            <ModalOverlay />
+            <ModalContent>
+              {surveyStep === 0 ? (
+                <>
+                  <ModalHeader>Realizar checkout</ModalHeader>
+                  <ModalBody>
+                    <Text>
+                      Para finalizar complete la siguiente encuesta de
+                      satisfacción
+                    </Text>
+                  </ModalBody>
+                  <ModalFooter justifyContent={'center'}>
+                    <Button onClick={handleNextSurveyStep}>
+                      Realizar encuesta
+                    </Button>
+                  </ModalFooter>
+                </>
+              ) : (
+                <>
+                  <ModalHeader>Encuesta de satisfacción</ModalHeader>
+                  <ModalBody>
+                    <Survey onClick={handleNextSurveyStep} step={surveyStep} />
+                  </ModalBody>
+                  <ModalFooter justifyContent={'center'}>
+                    <Button
+                      disabled={surveyStep < SURVEY.length}
+                      onClick={handleFinishSurvey}
+                    >
+                      Finalizar encuesta
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+        </Flex>
+      )}
       {bookingState === BookingState.Finalized && (
         <Flex
           direction={'column'}
@@ -406,9 +520,8 @@ const Home = () => {
           height={'100%'}
           flex={'1'}
         >
-          <Box>
-            <Text>Checkin realizado con éxito.</Text>
-          </Box>
+          <Text>Checkout realizado con éxito.</Text>
+          <Button onClick={() => {}}>Realizar checkout</Button>
         </Flex>
       )}
     </>
